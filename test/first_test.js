782cdde56ea34ea024b0a3eee99ba9797768bc8a
Evgenii Pisnyi <pisnyi@example.ru>
2020-02-15 20:06:04 +0200

    tst1

---
+++ test/first_test.js
@@ -0,0 +1,57 @@
+describe('Sign in to SSLs.com', function(){
+
+    const sign = $('[ng-click="$ctrl.toggleSearch()"]');
+    const search = $('.ssls-input');
+    const productCardHeader = $$('.ssls-product-card__header h3');
+
+    /* govno kod
+    
+    function findProducts(productCardHeader){
+        
+        for(let numberProduct = 0; numberProduct <= productCardHeader.length; numberProduct++){
+
+            let textProduct = productCardHeader.get(numberProduct).getText()
+
+            if (textProduct == 0) {
+                expect(textProduct).toEqual('PositiveSSL');
+            }
+            if (textProduct == 1) {
+                expect(textProduct).toEqual('PositiveSSL Wildcard');
+            }
+            if (textProduct == 2) {
+                expect(textProduct).toEqual('PositiveSSL Multi-Domain');
+            }
+
+        };
+
+    };
+    */
+
+    beforeAll(function(){
+        
+        browser.get('https://www.ssls.com/');
+
+    });
+
+    it('Click on the \'search\' button', function(){
+        sign.click();
+        expect(search.isPresent());
+    });
+
+    it('Page has correct title', function(){
+        expect(browser.getTitle()).toContain('Cheap SSL Certificates—Buy SSL Certs $3.77 & Save 52%');
+    });
+
+    it('Search \'PositiveSSL\' products', function(){
+        search.click();
+        search.sendKeys('PositiveSSL');
+        expect(search.getAttribute('value')).toBe('PositiveSSL');
+        
+        /*---need function---*/
+        expect(productCardHeader.get(0).getText()).toEqual('PositiveSSL');
+        expect(productCardHeader.get(1).getText()).toEqual('PositiveSSL Wildcard');
+        expect(productCardHeader.get(2).getText()).toEqual('PositiveSSL Multi-Domain');
+        /*---need function---*/ 
+    });
+
+});
