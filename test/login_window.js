57229395a4f84bbafcd55e871a1f5303b44959b8
Evgenii Pisnyi <pisnyi@example.ru>
2020-02-17 22:41:24 +0200

    added const for btn, inputs(email, password)

--- test/login_window.js
+++ test/login_window.js
@@ -1,3 +1,7 @@
-describe("", function(){
-    
+describe("Log in window", function(){
+
+    const logIn = $(".log-box a");
+    const email = $("form.email");
+    const password = $("form.password");
+
 });
